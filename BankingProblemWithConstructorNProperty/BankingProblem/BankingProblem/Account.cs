cc0189149422d037829abe8e1ba1a8b620bf2fa6
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-30 14:26:04 +0600

    succeed

---
+++ BankingProblemWithConstructorNProperty/BankingProblem/BankingProblem/Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProblem
+{
+    internal class Account
+    {
+        private string accNum;
+        private string custName;
+        private double balance;// = 0;
+
+        public Account()
+        {
+            balance = 0;
+        }
+
+        public Account(string accNum, string custName)
+        {
+            AccountNum = accNum;
+            CustName = custName;
+           // Balance = balance;
+        }
+        public string AccountNum
+        {
+            set
+            {
+                accNum = value;
+            }
+            get
+            {
+                return accNum;
+            }
+        }
+
+        public string CustName
+        {
+            set
+            {
+                custName = value;
+            }
+            get
+            {
+                return custName;
+            }
+
+        }
+
+        public double Balance 
+        {
+            get
+            {
+                return balance;
+            }
+        }
+        public double Deposit(double amount)
+        {
+            balance += amount;
+            return balance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            balance -= amount;
+            return balance;
+        }
+        
+    
+}
+}
